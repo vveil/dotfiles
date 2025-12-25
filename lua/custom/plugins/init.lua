9871d730b4c09ab6ab67a85ba8c146f2f9435971
Niklas <account@niklas.ai>
2025-12-25 19:09:28 +0100

    Squashed 'nvim/.config/nvim/' content from commit 5f18033
    git-subtree-dir: nvim/.config/nvim
    git-subtree-split: 5f180334e3bd3497b8f5b1b68538a66f86fd2e56

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,38 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+  },
+  {
+    'tpope/vim-dadbod',
+  },
+  {
+    'kristijanhusak/vim-dadbod-completion',
+    ft = { 'sql', 'mysql', 'plsql' },
+    lazy = true,
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      {
+        { 'kristijanhusak/vim-dadbod-completion', lazy = true },
+      },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  { 'sindrets/diffview.nvim' },
+}
