cc06ed23474c22be07ba41977f08863f43eccee8
Niklas Werner <niklas.werner@dampsoft.de>
2026-07-17 10:15:30 +0200

    some chezmoi improvements

--- dot_config/zsh/exact_dot_zshrc.d/60-work.zsh
+++ dot_config/zsh/exact_dot_zshrc.d/60-work.zsh
@@ -1,8 +1,20 @@
 [[ -r "$ZDOTDIR/.work-secrets.zsh" ]] && source "$ZDOTDIR/.work-secrets.zsh"
 
 if [[ -r "$HOME/devconfig/export_variables.sh" ]]; then
+  # devconfig registers a Bash-style completion for Vault.
+  if (( ! $+functions[complete] )); then
+    if (( $+functions[run-compinit] )); then
+      run-compinit
+    else
+      autoload -Uz compinit
+      compinit
+    fi
+    autoload -Uz bashcompinit
+    bashcompinit
+  fi
   source "$HOME/devconfig/export_variables.sh"
 fi
+
 if [[ -r "$HOME/devconfig/set_aliases.sh" ]]; then
   source "$HOME/devconfig/set_aliases.sh"
 fi
